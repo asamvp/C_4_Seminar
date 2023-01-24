97bc186ffd808dbd391036928ee4ad74a8790362
asam <asamvp@mail.ru>
2023-01-24 10:55:28 +0500

    Добавил ДЗ 1

---
+++ Homework_1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+int Prompt(string message)
+{
+    Console.Write($"{message} -> ");
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+int Step(int a, int b)
+{
+    int count = 1;
+    int result = a;
+    while (count < b)
+    {
+        result = result * a;
+        count++;
+    }
+    return result;
+}
+Console.Clear();
+int a = Prompt("Введите число A");
+int b = Prompt("Введите степень для числа А");
+Console.WriteLine($"Число {a} в степени {b} равно {Step(a, b)}");
